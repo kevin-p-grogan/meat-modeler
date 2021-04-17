cc071d77a1f91e4fc159027caf93c23a2bfd0dfa
Kevin Grogan <kevin.grogan@omni.sc>
2021-04-17 16:05:18 -0700

    Initial Commit

---
+++ MeatModeler/MeatModelerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeatModelerApp.swift
+//  MeatModeler
+//
+//  Created by Kevin Grogan on 4/17/21.
+//
+
+import SwiftUI
+
+@main
+struct MeatModelerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
