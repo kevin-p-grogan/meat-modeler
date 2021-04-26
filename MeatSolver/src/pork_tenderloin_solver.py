226542802217a8eac8a4b55c0799d8faec58f097
Kevin Grogan <kevin.grogan@omni.sc>
2021-04-25 19:21:26 -0700

    Completed 2D pork tenderloin modeling.

--- MeatSolver/src/pork_tenderloin_solver.py
+++ MeatSolver/src/pork_tenderloin_solver.py
@@ -1,58 +1,75 @@
 from tempfile import TemporaryDirectory
-from typing import Optional, Dict
+from typing import Optional
+import os
 
-import keras
 import tensorflow as tf
 import numpy as np
-from scipy.special import jv
+from scipy.special import j0, j1
 import matplotlib.pyplot as plt
 
 # Output parameters.
-NUM_BETA = 10
-NUM_RHO = 10
-NUM_TAU = 10
+NUM_BETA = 50
+NUM_RHO = 100
+NUM_TAU = 100
+NUSSELT_NUMBER = 50
 OUTPUT_FILEPATH = "model/pork_tenderloin.tflite"
-
-# Parameters for determining the transcendental equation. Parameters investigated in notebooks/transcendental_equation
-MIN_NUSSELT_NUMBER = 5.0
-MAX_NUSSELT_NUMBER = 500.0
-MIN_BETA = 0.0
-MAX_BETA = 100
-NUM_SAMPLES = 500
-
-# Base datatype for tensorflow
-DTYPE = tf.float32
 
 
 class PorkTenderloinModel:
     _num_beta: int
     _num_rho: int
     _num_tau: int
-    _model: Optional[keras.Model]
+    _nusselt_number: float
+    _model: Optional[tf.keras.Model]
 
-    def __init__(self, num_beta: int = 1, num_rho: int = 2, num_tau: int = 2):
+    # Parameters for determining the transcendental equation. Parameters investigated in notebooks/transcendental_equation
+    MIN_NUSSELT_NUMBER = 5.0
+    MAX_NUSSELT_NUMBER = 500.0
+    MIN_BETA = 0.0
+    MAX_BETA = 1000
+    NUM_SAMPLES = 500
+
+    # Parameters for the variables, tau and rho
+    MIN_TAU = 0.0
+    MAX_TAU = 1.0
+    MIN_RHO = 0.0
+    MAX_RHO = 0.5
+
+    # Base datatype for tensorflow
+    DTYPE = tf.float32
+
+    def __init__(self, num_beta: int = 1, num_rho: int = 2, num_tau: int = 2, nusselt_number: float = 50):
         self._num_beta = num_beta
         self._num_rho = num_rho
         self._num_tau = num_tau
+        self._nusselt_number = nusselt_number
         self._model = None
 
     def create(self):
-        input = tf.keras.Input(3, dtype=tf.float32)
-        kappa, theta, nusselt_number = (input[:, i] for i in range(3))
-        betas = self._compute_betas(nusselt_number)
-        pass  # TODO: Compute Theta
+        input = tf.keras.Input(2, dtype=self.DTYPE)
+        kappa = input[:, 0]
+        theta0 = input[:, 1]
+        betas = self._compute_betas()
+        rhos, taus = self._make_variables()
+        theta = self._compute_theta(rhos, taus, betas, kappa, theta0)
+        self._model = tf.keras.Model(inputs=input, outputs=theta)
 
-    def _compute_betas(self, nusselt_number: tf.Tensor) -> tf.Tensor:
+    def _compute_betas(self) -> np.ndarray:
         nusselt_numbers, betas = self._compute_zero_contours()
-        index = tf.argmin(tf.abs(nusselt_numbers - nusselt_number))  # Simple nearest neighbor.
+        index = np.argmin(np.abs(nusselt_numbers - self._nusselt_number))  # Simple nearest neighbor.
         return betas[:, index]
 
-    def _compute_zero_contours(self) -> (tf.Tensor, tf.Tensor):
-        nusselt_numbers = np.logspace(np.log10(MIN_NUSSELT_NUMBER), np.log10(MAX_NUSSELT_NUMBER), NUM_SAMPLES)
-        betas = np.linspace(MIN_BETA, MAX_BETA, NUM_SAMPLES)
+    def _compute_zero_contours(self) -> (np.ndarray, np.ndarray):
+        nusselt_numbers = np.logspace(
+            np.log10(self.MIN_NUSSELT_NUMBER),
+            np.log10(self.MAX_NUSSELT_NUMBER),
+            self.NUM_SAMPLES
+        )
+        betas = np.linspace(self.MIN_BETA, self.MAX_BETA, self.NUM_SAMPLES)
         nusselt_number_grid, beta_grid = np.meshgrid(nusselt_numbers, betas)
         residuals = PorkTenderloinModel._transcendental_equation(beta_grid, nusselt_number_grid)
         paths = plt.contour(nusselt_number_grid, beta_grid, residuals, levels=[0]).collections[0].get_paths()
+        plt.close("all")
         paths = sorted(paths, key=lambda p: p.vertices[:, 1].min())
         beta_contours = []
         assert len(paths) >= self._num_beta
@@ -65,14 +82,12 @@ class PorkTenderloinModel:
             beta_contour = np.interp(nusselt_numbers, nusselt_number_contour, beta_contour)
             beta_contours.append(beta_contour)
 
-        beta_contours = tf.convert_to_tensor(beta_contours, dtype=DTYPE)
-        nusselt_numbers = tf.convert_to_tensor(nusselt_numbers, dtype=DTYPE)
-        return nusselt_numbers, beta_contours
+        return nusselt_numbers, np.array(beta_contours)
 
     @staticmethod
     def _transcendental_equation(beta: np.ndarray, nusselt_number: np.ndarray) -> np.ndarray:
         """Yields the residual of the transcendental equation"""
-        residual = beta * jv(1, beta / 2.0) - nusselt_number * jv(0, beta / 2.0)
+        residual = beta * j1(beta / 2.0) - nusselt_number * j0(beta / 2.0)
         return residual
 
     def save(self, filepath: str):
@@ -86,8 +101,69 @@ class PorkTenderloinModel:
             with open(filepath, 'wb') as f:
                 f.write(tflite_model)
 
+    def _make_variables(self) -> (np.ndarray, np.ndarray):
+        """Create 1D tensors for rho and tau"""
+        taus = np.linspace(self.MIN_TAU, self.MAX_TAU, self._num_tau)
+        # sample the radius such that the volume is uniform
+        rhos = [self.MIN_RHO]
+        delta = (self.MAX_RHO**2.0-self.MIN_RHO**2.0) / (self._num_rho-1.0)
+        for index in range(self._num_rho-1):
+            rho = (rhos[index]**2.0 + delta)**0.5
+            rhos.append(rho)
+        rhos = np.array(rhos)
+        return rhos, taus
+
+    @staticmethod
+    def _compute_theta(
+            rhos: np.ndarray,
+            taus: np.ndarray,
+            betas: np.ndarray,
+            kappa: tf.Tensor,
+            theta0: tf.Tensor) -> tf.Tensor:
+
+        taus = np.reshape(taus, (-1, 1, 1))
+        rhos = np.reshape(rhos, (1, -1, 1))
+        betas = np.reshape(betas, (1, 1, -1))
+        temporal_decay = tf.exp(-betas ** 2.0 * kappa * taus)
+        radial_component = j0(betas * rhos)
+        amplitudes_numerator = 4.0 * theta0 * j1(betas / 2)
+        amplitudes_denominator = betas * (j1(betas / 2) ** 2.0 + j0(betas / 2) ** 2.0)
+        amplitudes = amplitudes_numerator / amplitudes_denominator
+        theta = amplitudes * radial_component * temporal_decay
+        theta = tf.reduce_sum(theta, axis=-1)
+        return theta
+
+    def plot(self, kappa: float, theta0: float, output_directory: Optional[str] = None):
+        assert self._model
+        inputs = tf.convert_to_tensor([kappa, theta0], dtype=tf.float32)[tf.newaxis, ...]
+        thetas = self._model.predict(inputs)
+        rhos, taus = self._make_variables()
+        num_tau, num_rho = thetas.shape
+        plt.close("all")
+        plt.figure(1)
+        for i in range(num_tau):
+            plt.plot(rhos, thetas[i, :], label=f"{taus[i]}")
+        title = f"kappa={kappa}, theta0={theta0}"
+        plt.title(title)
+        plt.xlabel("rho")
+        plt.ylabel("theta")
+        plt.legend(loc="best")
+        if output_directory:
+            plt.savefig(os.path.join(output_directory, title + ".png"))
+
+
+        plt.figure(2)
+        for j in range(num_rho):
+            plt.plot(taus, thetas[:, j], label=f"{rhos[j]}")
+        plt.title(f"kappa={kappa}, theta0={theta0}")
+        plt.xlabel("tau")
+        plt.ylabel("theta")
+        plt.legend(loc="best")
+        if output_directory:
+            plt.savefig(os.path.join(output_directory, title + ".png"))
+
 
 if __name__ == "__main__":
-    pork_tenderloin_model = PorkTenderloinModel(NUM_BETA, NUM_RHO, NUM_TAU)
+    pork_tenderloin_model = PorkTenderloinModel(NUM_BETA, NUM_RHO, NUM_TAU, NUSSELT_NUMBER)
     pork_tenderloin_model.create()
-    pork_tenderloin_model.save("model/pork_tenderloin.tflite")
+    pork_tenderloin_model.save("../model/pork_tenderloin.tflite")
