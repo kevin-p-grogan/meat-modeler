558b583428e5861ad65554b7c77d293605e90b89
Kevin Grogan <kevin.grogan@omni.sc>
2021-06-18 14:01:42 -0400

    Added domain creation.

--- MeatSolver/src/flat_frustum_model_maker.py
+++ MeatSolver/src/flat_frustum_model_maker.py
@@ -1,28 +1,54 @@
 from typing import Dict
+from math import pi
 
 import tensorflow as tf
+import numpy as np
 
 from src.model import ModelMaker
 
 
 class FlatFrustumModelMaker(ModelMaker):
-    _kappa: tf.Tensor
-    _tau: tf.Variable
-    _dtau: tf.constant
+    # Runtime Parameters
+    _diffusivity: tf.Tensor
+    _nusselt_number: tf.Tensor
+    _theta0: tf.Tensor
+    _aspect_ratio: tf.Tensor
+    _pointiness: tf.Tensor
+    _flatness: tf.Tensor
+    _radial_bc_constants: tf.Tensor
+
+    # Model definition-time variables
+    _rho: tf.Tensor
+    _phi: tf.Tensor
+    _xi: tf.Tensor
     _num_rho: int
-    _num_theta: int
+    _num_phi: int
     _num_xi: int
 
-    # simulation parameters
-    NUM_RUNTIME_PARAMETERS = 2
+    # Runtime variables
+    _tau: tf.Variable
+    _dtau: tf.constant
+
+    # simulation constants
+    NUM_RUNTIME_PARAMETERS = 6
     NUM_TAU = 100
     MIN_TAU = 0.0
     MAX_TAU = 1.0
+    MIN_RHO = 0.0
+    MAX_RHO = 0.5
+    MIN_PHI = 0.0
+    MAX_PHI = 2.0 * pi
+    MIN_XI = 0.0
+    DTYPE = tf.float32
+    NUM_GHOST = 1
+    NUM_DIMS = 3
 
-    def __init__(self, num_rho: int = 2, num_theta: int = 2, num_xi: int = 2):
+    def __init__(self, num_rho: int = 2, num_phi: int = 2, num_xi: int = 2):
         self._num_rho = num_rho
-        self._num_theta = num_theta
+        self._num_phi = num_phi
         self._num_xi = num_xi
+        self._tau = tf.Variable(self.MIN_TAU)
+        self._dtau = tf.constant((self.MAX_TAU - self.MIN_TAU) / self.NUM_TAU, dtype=self.DTYPE)
 
     def make(self):
         parameters = tf.keras.Input(self.NUM_RUNTIME_PARAMETERS, dtype=self.DTYPE, batch_size=1)
@@ -30,14 +56,57 @@ class FlatFrustumModelMaker(ModelMaker):
         for _ in range(self.NUM_TAU):
             theta = self._advance(theta)
 
+        theta = self._finalize(theta)
         self._model = tf.keras.Model(inputs=parameters, outputs=theta)
 
     def _initialize(self, parameters: tf.Tensor) -> (tf.Tensor, tf.Variable):
-        self._kappa = parameters[0, 0]
-        self._tau = tf.Variable(self.MIN_TAU)
-        self._dtau = tf.constant((self.MAX_TAU - self.MIN_TAU) / self.NUM_TAU, dtype=self.DTYPE)
-        theta = parameters[0, 1] * tf.ones((self._num_rho, self._num_theta, self._num_xi), dtype=self.DTYPE)
+        self._set_runtime_parameters(parameters)
+        self._set_domain_variables()
+        self._set_boundary_constants()
+        theta = self._create_theta()
         return theta
+
+    def _set_runtime_parameters(self, parameters: tf.Tensor):
+        self._diffusivity = parameters[0, 0]
+        self._nusselt_number = parameters[0, 1]
+        self._theta0 = parameters[0, 2]
+        self._aspect_ratio = parameters[0, 3]
+        self._pointiness = parameters[0, 4]
+        self._flatness = parameters[0, 5]
+
+    def _create_theta(self) -> tf.Tensor:
+        shape = self._num_rho, self._num_phi, self._num_xi
+        theta = self._theta0 * tf.ones(shape, dtype=self.DTYPE)
+        paddings = [[self.NUM_GHOST, self.NUM_GHOST]] * len(shape)
+        theta = tf.pad(theta, paddings)  # zero pad the boundaries
+        return theta
+
+    def _set_domain_variables(self):
+        """Defined cell-centered coordinates for the domain variables"""
+        min_rho, max_rho = tf.constant(self.MIN_RHO), tf.constant(self.MAX_RHO)
+        self._rho = self._create_domain(lower=min_rho, upper=max_rho, num_cells=self._num_rho, axis=0)
+        min_phi, max_phi = tf.constant(self.MIN_PHI), tf.constant(self.MAX_PHI)
+        self._phi = self._create_domain(lower=min_phi, upper=max_phi, num_cells=self._num_phi, axis=1)
+        min_xi, max_xi = tf.constant(self.MIN_XI), self._aspect_ratio
+        self._xi = self._create_domain(lower=min_xi, upper=max_xi, num_cells=self._num_xi, axis=2)
+
+    def _set_boundary_constants(self):
+        pass
+
+    def _create_domain(self,
+                       lower: tf.Tensor,
+                       upper: tf.Tensor,
+                       num_cells: int,
+                       axis: int) -> tf.Tensor:
+        """Creates a cell-centered grid with ghost cells."""
+        delta = (upper - lower) / float(num_cells)
+        num_total_cells = num_cells + 2*self.NUM_GHOST
+        start = lower - delta/2.0
+        end = upper + delta/2.0
+        domain = start + (end-start)*np.linspace(0.0, 1.0, num_total_cells)
+        shape = [num_total_cells if ax == axis else 1 for ax in range(self.NUM_DIMS)]
+        domain = tf.reshape(domain, shape=shape)
+        return domain
 
     def _advance(self, theta: tf.Tensor):
         theta = self._set_boundary(theta)
@@ -50,10 +119,14 @@ class FlatFrustumModelMaker(ModelMaker):
         return theta
 
     def _compute_gradients(self, theta: tf.Tensor) -> Dict[str, tf.Tensor]:
-        # TODO: compute gradiebnts
+        # TODO: compute gradients
         return {}
 
     def _update(self, theta: tf.Tensor, gradients: Dict[str, tf.Tensor]) -> tf.Tensor:
-        theta = theta + self._dtau * self._kappa * theta
+        theta = theta + self._dtau * self._diffusivity * theta
         self._tau = self._tau + self._dtau
         return theta
+
+    @staticmethod
+    def _finalize(theta: tf.Tensor) -> tf.Tensor:
+        return theta[1:-1, 1:-1, 1:-1]
--- MeatSolver/test/test_flat_frustum_model_maker.py
+++ MeatSolver/test/test_flat_frustum_model_maker.py
@@ -1,5 +1,6 @@
 import unittest
 
+import numpy as np
 import tensorflow as tf
 
 from src.flat_frustum_model_maker import FlatFrustumModelMaker
@@ -13,6 +14,26 @@ class TestFlatFrustumModelMaker(unittest.TestCase):
         flat_frustum_model_maker.make()
         self.assertIsInstance(flat_frustum_model_maker._model, tf.keras.Model)
         flat_frustum_model_maker.save(self._test_model_filepath)
+
+    def test_create_domain(self):
+        model_maker = FlatFrustumModelMaker()
+        lower = tf.constant(0.0)
+        upper = tf.constant(1.0)
+        num_cells = 10
+        for ax in range(model_maker.NUM_DIMS):
+            domain = model_maker._create_domain(lower, upper, num_cells, ax)
+            rank = len(domain.shape)
+            self.assertEqual(rank, model_maker.NUM_DIMS)
+            self.assertLessEqual(tf.reduce_min(domain), lower)
+            self.assertGreaterEqual(tf.reduce_max(domain), upper)
+            self.assertEqual(np.argmax(domain.shape), ax)
+            non_axis_dimensions_are_singletons = all(d == 1 for i, d in enumerate(domain.shape) if i != ax)
+            self.assertTrue(non_axis_dimensions_are_singletons)
+
+        # Check to see that this works for a KerasTensor input
+        lower = tf.keras.Input(3, batch_size=1)[0, 0]
+        domain = model_maker._create_domain(lower, upper, num_cells, 0)
+        self.assertEqual(np.argmax(domain.shape), 0)
 
 
 if __name__ == '__main__':
